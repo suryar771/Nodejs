5005f06c6be44e016c9ae60ea5f59bf3711543df
suryar771 <suri624177@gmail.com>
2025-06-10 23:35:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+ 
+const server = http.createServer((req, res) => {
+    console.log("New req");
+    res.end("Hello World");
+});
+
+server.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
+
+
