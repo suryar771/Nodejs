41859e8edc58f43b513764e9312bc4eb3bccc729
suryar771 <suri624177@gmail.com>
2025-06-14 21:36:38 +0530

    restful api

--- project-01/index.js
+++ project-01/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const users = require("./MOCK_DATA.json");
 
 const port = 8000;
-app.listen(port, () => {console.log(`Server is running on port ${port}`)});
+app.get('/users', (req,res) => {
+    res.json(users);
+});
+
+
+app.listen(port, () => {console.log(`Server is running on port ${port}`)}); 
