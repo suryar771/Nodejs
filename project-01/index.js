e332fd768cddbb1c3970a9092facb1ab34fc37ac
suryar771 <suri624177@gmail.com>
2025-06-14 21:54:33 +0530

    dynamic path req

--- project-01/index.js
+++ project-01/index.js
@@ -4,8 +4,23 @@ const users = require("./MOCK_DATA.json");
 
 const port = 8000;
 app.get('/users', (req,res) => {
-    res.json(users);
+    const html = `
+   
+    <ul>
+        ${users.map((user) => `<li>${user.first_name} ${user.last_name}</li>`).join('')}
+    </ul>
+    `;
+    return res.send(html);
+});
+app.get('/api/users/:id',(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+
+});
+app.get('/api/users', (req,res) => {
+   return res.json(users);
 });
 
 
-app.listen(port, () => {console.log(`Server is running on port ${port}`)}); 
+app.listen(port, () => {console.log(`Server is running on port ${port}`)});   
