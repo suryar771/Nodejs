3a004699802e4db83a5c73f907dcd2939ec66083
suryar771 <suri624177@gmail.com>
2025-06-18 22:34:25 +0530

    post req

--- http server/index.js
+++ http server/index.js
@@ -3,14 +3,22 @@ const fs = require("fs")
  
 const server = http.createServer((req, res) => {
     fs.appendFile("log.txt", "New req \n", (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(data);
+    });
        switch(req.url) {
         case "/":
             res.end("Hello World");
             break;
         case "/about":
             res.end("About");
-       }
-    });
+             break;
+       default:
+        res.end("404 Not Found");
+        break;
+    }
 });
 
 server.listen(3000, () => {
--- project-01/index.js
+++ project-01/index.js
@@ -12,11 +12,18 @@ app.get('/users', (req,res) => {
     `;
     return res.send(html);
 });
-app.get('/api/users/:id',(req,res)=>{
+app.route('/api/users/:id').get((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
     return res.json(user);
 
+}).patch((req,res)=>{
+    return res.json({status: "pending"});
+}).delete((req,res)=>{
+    return res.json({status: "pending"});
+});
+app.post('/api/users',(req,res)=>{
+    return res.json({status: "pending"});
 });
 app.get('/api/users', (req,res) => {
    return res.json(users);
