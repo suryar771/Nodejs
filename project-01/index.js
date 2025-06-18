1e5cf7364a4e829e10fb87545c7c50dc0e1b9e20
suryar771 <suri624177@gmail.com>
2025-06-18 23:09:33 +0530

    post req testing

--- project-01/index.js
+++ project-01/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const port = 8000;
+app.use(express.urlencoded({extended: false}));
+
 app.get('/users', (req,res) => {
     const html = `
    
@@ -12,7 +15,8 @@ app.get('/users', (req,res) => {
     `;
     return res.send(html);
 });
-app.route('/api/users/:id').get((req,res)=>{
+app.route('/api/users/:id')
+.get((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
     return res.json(user);
@@ -23,7 +27,12 @@ app.route('/api/users/:id').get((req,res)=>{
     return res.json({status: "pending"});
 });
 app.post('/api/users',(req,res)=>{
-    return res.json({status: "pending"});
+    const body = req.body;
+    users.push({...body,id:users.length +1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+       
+        return res.json({status: "success"});
+    });
 });
 app.get('/api/users', (req,res) => {
    return res.json(users);
