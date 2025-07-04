bb7aeb3418255380264308a513828aba8cce55dd
suryar771 <suri624177@gmail.com>
2025-07-04 23:31:36 +0530

    mvc routes

--- project-01/index.js
+++ project-01/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
+const connectDB = require('./connection');
 
-const mongoose = require("mongoose");
 const fs = require("fs");
 const userRouter = require('./routes/user');
 const port = 8000;
-mongoose.connect('mongodb://127.0.0.1:27017/project-01')
-.then(()=> console.log("MongoDB Connected"))
-.catch(err => console.log("Mongo Error",err))
+
+connectDB('mongodb://127.0.0.1:27017/project-01');
 
 
 app.use(express.urlencoded({extended: false}));
