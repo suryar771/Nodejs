1ab3046898e657ba8c278e8bcabb19d86bbc2ae3
suryar771 <suri624177@gmail.com>
2025-07-16 19:55:51 +0530

    Added a test route

--- urlshortner/index.js
+++ urlshortner/index.js
@@ -3,16 +3,23 @@ const express = require("express")
 const connectToMongoDB = require("./connect");
 const URL = require('./models/url');
 const urlRoute = require('./router/url');
+const path = require("path");
 
 connectToMongoDB('mongodb://127.0.0.1:27017/short-url').then(()=> console.log("Mongodb is successfully connected"));
 
 const app = express();
 
 app.set("view engine","ejs");
+app.set('views',path.resolve("./views"));
 
 const PORT = 8001;
 
 app.use(express.json());
+
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render("home", { urls: allUrls });
+});
 
 app.use("/url",urlRoute);
 app.get('/:shortId', async (req, res) => {
@@ -26,7 +33,12 @@ app.get('/:shortId', async (req, res) => {
             }
         }
     );
-    res.redirect(entry.redirectURL)
+
+    if (!entry) {
+        return res.status(404).send("Short URL not found");
+    }
+
+    res.redirect(entry.redirectURL);
 });
 
 
